9c0249b6dc53c0edddf61f3ed4b0f530e20b55f4
Ronald Cifuentes <ronald.cifuentes@externos-cl.cencosud.com>
2023-07-19 18:10:57 -0500

    feat: coverage 100%

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,9 +1,15 @@
 import { FC } from 'react'
 import { AppProps } from './interfaces'
 import './App.css'
+import GlobalStyle from './App.styled'
 
 const App: FC<AppProps> = ({ dataTestId = 'app' }) => {
-  return <div data-testid={dataTestId}>Hi</div>
+  return (
+    <div data-testid={dataTestId}>
+      <GlobalStyle />
+      Hi
+    </div>
+  )
 }
 
 export default App
