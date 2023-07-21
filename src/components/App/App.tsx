07c36494f3fccb1c3657e5d272146c42cc11bc91
Ronald Cifuentes <ronald.cifuentes@externos-cl.cencosud.com>
2023-07-20 19:24:09 -0500

    🧑‍💻 Feat: add Redux

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,9 +1,15 @@
 import { FC } from 'react'
 import { AppProps } from './interfaces'
 import { AppContainer } from './App.styled'
+import Counter from '../Counter'
 
 const App: FC<AppProps> = ({ dataTestId = 'app' }) => {
-  return <AppContainer data-testid={dataTestId}>Hi</AppContainer>
+  return (
+    <AppContainer data-testid={dataTestId}>
+      <h1>Hi</h1>
+      This is a Redux Counter: <Counter />
+    </AppContainer>
+  )
 }
 
 export default App
